0f30c8b9e2eccff53b4afa1db46868b574321bf5
EVERYSAN <128579465+EVERYSAN@users.noreply.github.com>
2025-09-29 06:02:21 +0900

    Delete lib directory

--- lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from 'stripe';
-
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-    apiVersion: "2025-08-27.basil", // ここは必ず最新verにする
-    typescript: true
-})
--- lib/utils.ts
+++
@@ -1,3 +0,0 @@
-export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLICK_APP_URL}${path}`
-}
